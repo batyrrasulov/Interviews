268d25148ddce25fbd1e05e448494eeac5fff19c
Dave Gooden <Dave.Gooden@calero.com>
2023-08-08 08:28:48 -0400

    #226651 Moves Core TEM interview problems into git from wiki

---
+++ Calculator/Interview/Calculators/RangeCalculator.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Interview
+{
+    public class RangeCalculator : ICalculator
+    {
+        public string Caption => "Range";
+
+        public double Calculate(int[] inputs)
+        {
+            int minimum = 0;
+            int maximum = 0;
+            foreach (int i in inputs)
+            {
+                if (i < minimum)
+                    minimum = i;
+                else if (i > maximum)
+                    maximum = i;
+            }
+            return maximum - minimum;
+        }
+    }
+}
---
+++ Calculator/Interview/ICalculator.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Interview
+{
+    public interface ICalculator
+    {
+        string Caption { get; }
+        double Calculate(int[] inputs);
+    }
+
+    public class MedianCalculator : ICalculator
+    {
+        public string Caption => "Median";
+
+        public double Calculate(int[] inputs)
+        {
+            return inputs[inputs.Length / 2];
+        }
+    }
+}
